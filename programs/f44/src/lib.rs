8c58a2b9cadd37fceeba144f69fe6aebcf5e93e8
0xcrypt102 <tiensucha5@gmail.com>
2025-01-22 20:15:12 +0000

    add events

--- programs/f44/src/lib.rs
+++ programs/f44/src/lib.rs
@@ -2,12 +2,14 @@ pub mod constants;
 pub mod error;
 pub mod instructions;
 pub mod state;
+pub mod events;
 
 use anchor_lang::prelude::*;
 
 pub use constants::*;
 pub use instructions::*;
 pub use state::*;
+pub use events::*;
 
 declare_id!("C67YgrXJvyN9AY8jE6yLccNDNfNxg9LKS7LTJDKsMqnv");
 
