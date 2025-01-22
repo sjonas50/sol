4331a130f477fc62b74b997be427a69418d1dd21
0xcrypt102 <tiensucha5@gmail.com>
2025-01-22 20:13:53 +0000

    add error code

--- programs/f44/src/error.rs
+++ programs/f44/src/error.rs
@@ -1,7 +1,37 @@
 use anchor_lang::prelude::*;
 
 #[error_code]
-pub enum ErrorCode {
-    #[msg("Custom error message")]
-    CustomError,
+pub enum F44Code {
+    #[msg("The given account is not authorized to execute this instruction.")]
+    NotAuthorized,
+
+    #[msg("The given account is not valid fee recipient.")]
+    UnValidFeeRecipient,
+
+    #[msg("The program is already initialized.")]
+    AlreadyInitialized,
+
+    #[msg("slippage: Too much SOL required to buy the given amount of tokens.")]
+    TooMuchSolRequired,
+
+    #[msg("slippage: Too little SOL received to sell the given amount of tokens.")]
+    TooLittleSolReceived,
+
+    #[msg("The mint does not match the bonding curve.")]
+    MintDoesNotMatchBondingCurve,
+
+    #[msg("The bonding curve has completed and liquidity migrated to raydium.")]
+    BondingCurveComplete,
+
+    #[msg("The bonding curve has not completed.")]
+    BondingCurveNotComplete,
+
+    #[msg("The program is not initialized.")]
+    NotInitialized,
+    
+    #[msg("Math operation overflow.")]
+    MathOverflow,
+
+    #[msg("Amount should be bigger than 0.")]
+    ZeroAmount
 }
