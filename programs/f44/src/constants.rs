32908a270160ae815434c400652a43f1ece9d209
0xcrypt102 <tiensucha5@gmail.com>
2025-01-22 20:13:18 +0000

    add constants

--- programs/f44/src/constants.rs
+++ programs/f44/src/constants.rs
@@ -1,4 +1,8 @@
 use anchor_lang::prelude::*;
 
 #[constant]
-pub const SEED: &str = "anchor";
+pub const GLOBAL_STATE_SEED: &[u8] = b"GLOBAL-STATE-SEED";
+pub const MINT_SEED: &[u8] = b"mint";
+pub const BONDING_CURVE: &[u8] = b"BONDING-CURVE";
+pub const VAULT_SEED: &[u8] = b"VAULT-SEED";
+pub const SOL_VAULT_SEED: &[u8] = b"SOL-VAULT-SEED";
