9c9b4619ba26e0118afd94c599f4b0812dbe870e
0xcrypt102 <tiensucha5@gmail.com>
2025-01-27 21:52:53 +0000

    feat: update params in set_params

--- programs/f44/src/instructions/set_params.rs
+++ programs/f44/src/instructions/set_params.rs
@@ -18,43 +18,29 @@ pub struct SetParams<'info> {
     pub user: Signer<'info>,
 }
 
-pub fn set_params(ctx: Context<SetParams>, fee_recipient: Pubkey,owner_wallet: Pubkey,  initial_virtual_token_reserves: u64, initial_virtual_sol_reserves: u64, initial_real_token_reserves: u64, token_total_supply: u64, mcap_limit: u64, fee_basis_points: u64, create_fee: u64) -> Result<()> {
+pub fn set_params(ctx: Context<SetParams>, fee_recipient: Pubkey,owner_wallet: Pubkey, mcap_limit: u64, fee_basis_points: u64, create_fee: u64) -> Result<()> {
     let global = &mut ctx.accounts.global;
 
     require!(global.authority == ctx.accounts.user.key(), F44Code::NotAuthorized);
 
     global.fee_recipient = fee_recipient;
     global.owner_wallet = owner_wallet;
-    global.initial_virtual_token_reserves = initial_virtual_token_reserves;
-    global.initial_virtual_sol_reserves = initial_virtual_sol_reserves;
-    global.initial_real_token_reserves = initial_real_token_reserves;
-    global.token_total_supply = token_total_supply;
     global.mcap_limit = mcap_limit;
     global.fee_basis_points = fee_basis_points;
     global.create_fee = create_fee;
-    global.authority = ctx.accounts.user.key();
 
     msg!("Set params\n
         fee recipient is {:?}\n
         owner wallet is {:?}\n
-        initial virtual token reserves is {:?}\n
-        initial virtual sol reserves is {:?}\n
-        initial real token reserves is {:?}\n
-        token total supply is {:?}\n
         mcap limit is {:?}\n
         fee basis points is {:?}\n
         create fee is {:?}\n
         authority {:?}", 
         global.fee_recipient,
         global.owner_wallet,
-        global.initial_virtual_token_reserves,
-        global.initial_virtual_sol_reserves,
-        global.initial_real_token_reserves,
-        global.token_total_supply,
         global.mcap_limit,
         global.fee_basis_points,
         global.create_fee,
-        global.authority,
     );
 
     Ok(())
